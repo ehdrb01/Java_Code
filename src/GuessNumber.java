ab214f4f2560ea191f40a4bf02108e855bef4bb6
김동규 <kdg1941@naver.com>
2023-05-11 00:38:05 +0900

    update GuessNumber

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -21,22 +21,20 @@ public class GuessNumber {
      *               otherwise return 0
      * int guess(int num);
      */
-        public int guessNumber(int n) {
-            long s = 0;
-            long e = n;
-            long mid=(s+e)/2;
-            while(s<=e){
-                mid=(s+e)/2;
-                int temp=guess((int)mid);
-                if(temp==0)
-                    return (int)mid;
-                else if(temp==1)
-                    s=mid+1;
-                else
-                    e =mid-1;
+    public int guessNumber(int n) {
+        long s = 0;
+        long e = n;
+        long mid=(s+e)/2;
+        while(s<=e){
+            mid=(s+e)/2;
+            int temp=guess((int)mid);
+            if(temp==0)
+                return (int)mid;
+            else if(temp==1)
+                s=mid+1;
+            else
+                e =mid-1;
             }
             return (int)mid;
         }
-    }
-
 }
